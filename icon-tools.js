668015054ce807d03a4113b07e90d314812b3f42
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-24 10:58:01 -0500

    Update icon-tools.js

--- icon-tools.js
+++ icon-tools.js
@@ -9,3 +9,21 @@
             }
             link.href = url;
   }
+
+function svgIcon(icon){
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="50%" y="45%" font-size="60" text-anchor="middle" dy=".3em">${icon}</text></svg>`;
+}
+
+
+function dataURI(options){
+  const {content,mime,base64} = options;
+  return `data:${mime};${base64?'base64,':''}${content}`;
+}
+
+const svg = (dataURI({content:svgIcon('Q'),mime:'image/svg+xml'}));
+
+let a = document.createElement('a');
+
+a.href = svg;
+
+a.click();

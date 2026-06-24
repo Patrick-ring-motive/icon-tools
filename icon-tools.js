e980da92ca4dddd7018cab7f198f8c4cf8b468b7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-24 11:08:25 -0500

    Enhance svgIcon function with hue filter and decodeU

--- icon-tools.js
+++ icon-tools.js
@@ -11,14 +11,37 @@
   }
 
 function svgIcon(icon){
-  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text x="50%" y="45%" font-size="60" text-anchor="middle" dy=".3em">${icon}</text></svg>`;
+  return `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <defs>
+    <filter id="hue">
+      <feColorMatrix type="hueRotate" values="180"/>
+    </filter>
+  </defs>
+
+  <text
+    x="50%"
+    y="45%"
+    font-size="60"
+    text-anchor="middle"
+    dy=".3em"
+    filter="url(#hue)"
+  >${icon}</text>
+</svg>`;
 }
 
+const decodeU = x =>{
+  try{
+    return decodeURIComponent(x);
+  }catch{
+    return x;
+  }
+};
 
 function dataURI(options){
   const {content,mime,base64} = options;
-  return `data:${mime}${base64?';base64':''},${content}`;
+  return `data:${mime}${base64?';base64':''},${encodeURIComponent(decodeU(content))}`;
 }
 
-const svg = (dataURI({content:svgIcon('Q'),mime:'image/svg+xml'}));
+const svg = (dataURI({content:svgIcon('🐢'),mime:'image/svg+xml'}));
 setFavicon(svg)
